db28c10b7fbd84dc9df7e8bd092f9aafbb04a6cb
boutique-frontend <sanajohnfatorma@gmail.com>
2026-08-26 02:56:57 +0000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,6 @@
 const CONFIG = {
-    API_URL: "http://localhost:5000/api",
+    APP_NAME: "SAnA",
+    CURRENCY_SYMBOL: "Rs.",
     ADMIN_PASSCODE: "1234",
-    WHATSAPP_NUMBER: "23200000000" // Change to her WhatsApp number
+    WHATSAPP_NUMBER: "923000000000" // Replace with her Pakistani WhatsApp number (+92 format)
 };
